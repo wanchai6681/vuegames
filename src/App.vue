d0cc2d41beef712d9cd90779be5c25daf6e16206
Wanchai Nool <wanchainool@MacBook-Pro-khxng-Wanchai.local>
2020-08-28 18:42:10 +0700

    git

--- src/App.vue
+++ src/App.vue
@@ -1,231 +1,31 @@
 <template>
-<div id="app" class="container">
-
-    <div class="fullscreen-viedo-wrap ">
-        <video src="" loop autoplay muted></video>
-    </div>
-    <div class="overlay"></div>
-
-    <div class="row mt-5">
-        <div class="col-md-4"></div>
-        <div class="col-md-4 content "> <button @click="show = !show" class="btn btn-danger  mb-3 ">Start</button>
-
-            <transition name="fade">
-                <div v-if="show " class="content mb-3">
-                    <Wan @Imgmonster="imgmonster" @Imghero="imghero" @Heroname="Heroname" @HeroHP="HeroHp" @Monstername="Monstername" @MonsterHP="MonsterHP"></Wan>
-                </div>
-
-            </transition>
-        </div>
-
-        <div class="col-md-4"></div>
-    </div>
-       <div class="row d-flex justify-content-center">
-        <div class="col-md-4">
-
-        </div>
-        <div class="col-md-4 content ">
-            <Wbutton @atk="Attack" @SPATK="$SPAttack" @RandomATK="Attackback" @RandomSPATK="SPAttackback"> </Wbutton>
-
-        </div>
-        <div class="col-md-4">
-
-        </div>
-
-    </div>
-
-    <div class="row d-flex mb-5">
-
-        <div class=" col-md-5 content">
-            <h1>{{heroname}} </h1>
-            <div class="d-flex justify-content-center">
-                <H5>HP :</H5>
-                <b-progress :value="HPHero" :max="HPHeromax" show-value variant="danger" height="25px" class="w-75 md-2  ml-2"></b-progress>
-            </div>
-
-        </div>
-        <div class="col-md-2 content"></div>
-        <div class="col-md-5 content">
-            <h1>{{monstername}}</h1>
-            <div class="d-flex justify-content-center">
-                <H5>HP :</H5>
-                <b-progress :value="HPMonster" :max="HPMonstermax" show-value variant="danger" height="25px" class="w-75 md-2 ml-2  "></b-progress>
-            </div>
-        </div>
-
-    </div>
-    <div class="row ">
-        <div class="col-md-3"></div>
-        <div class="col-md-6 content">
-        </div>
-        <div class="col-md-3"></div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-5 content d-flex justify-content-center">
-            <p v-if="imgHero" v-bind:style="{width: fsizeH_W + 'px' , height: fsizeH_H + 'px'}"><img :src="imgHero" alt="" class="img-fluid"></p>
-
-        </div>
-
-        <div class="col-md-2 content flex-column">
-            
-            <div class="p-5">
-                <h1>VS</h1>
-            </div>
-
-        </div>
-
-        <div class="col-md-5 content d-flex justify-content-center">
-            <p v-if="imgMonster"  v-bind:style="{ width: fsizeM_W + 'px' , height: fsizeM_H + 'px'}"><img :src="imgMonster" alt="" class="img-fluid"></p>
-
-        </div>
-
-    </div>
-
- 
-  
-            <Mwan v-if="HPHero <= 0 && HPMonster > 0 && heroname != ''"  @$reset="$reset" @reset="reset" @close="showModal = false"  >
-                <h3 slot="header">YOU LOUSE</h3>
-            </Mwan>
-            <Mwan v-if="HPMonster <= 0 && HPHero > 0 && heroname != ''"  @$reset="$reset" @reset="reset" @close="showModal = false"  >
-                <h3 slot="header">YOU WIN</h3>
-            </Mwan>
-            <Mwan v-if="(HPHero<=0 && HPMonster <= 0 ) && heroname != ''"  v-bind="reset" @$reset="$reset" @reset="reset" @close="showModal = false"  >
-                <h3 slot="header">DRAW</h3>
-            </Mwan>
-
-</div>
+  <div id="app">
+    <HelloWorld msg="" />
+    <wbutton></wbutton> />
+  </div>
 </template>
 
 <script>
-import Wbutton from "./components/Wbutton"
-import Wan from "./components/Wan.vue"
-import Mwan from "./components/Mwan.vue"
-import ''
+import HelloWorld from "./components/HelloWorld.vue";
+import Wbutton from "./components/Wbutton";
 
 export default {
-    name: 'App',
-    el: "#App",
-
-    prop: {
-
-    },
-
-    components: {
-        Wbutton,
-        Wan,
-        Mwan
-
-    },
-
-    data: function () {
-        return {
-            show: false,
-            showModal: false,
-            n: 0,
-            HPHero: 0,
-            heroname: "",
-            monstername: "",
-            HPMonster: 0,
-            atk: 0,
-            imgHero: '',
-            imgMonster: '',
-            HPHeromax: 0,
-            max: 0,
-            HPMonstermax: 0,
-            fsizeM_W: 500,
-            fsizeH_W : 500,
-            fsizeM_H: 300,
-            fsizeH_H : 300
-
-
-        }
-
-    },
-    methods: {
-
-        Heroname(value) {
-            this.heroname = value
-            console.log("emit", this.heroname)
-        },
-        HeroHp(value) {
-            this.HPHero = value
-            this.HPHeromax = value
-            this.fsizeH_H = value*2
-            this.fsizeH_W = value*2
-            console.log("emit", this.HPHero)
-        },
-        Monstername(value) {
-            this.monstername = value
-            console.log("emit m", this.monstername)
-        },
-
-        MonsterHP(value) {
-            this.HPMonster = value
-            this.HPMonstermax = value
-            this.fsizeM_H = value*2
-            this.fsizeM_W = value*2
-            console.log("emit mhp", this.HPMonster)
-        },
-        Attack(value) {
-            this.atk = value
-            this.fsizeM_W -= value*1.5
-            this.fsizeM_H -= value*1.5
-            this.HPMonster -= this.atk
-        },
-
-        $SPAttack(value) {
-            this.atk = value
-            this.fsizeM_W -= value
-            this.fsizeM_H -= value
-            this.HPMonster -= this.atk
-        },
-        Attackback(value) {
-            this.atk = value
-            this.fsizeH_W -= value*1.5
-            this.fsizeH_H -= value*1.5
-            this.HPHero -= this.atk
-        },
-
-        SPAttackback(value) {
-
-            this.atk = value
-            this.fsizeH_W -= value
-            this.fsizeH_H -= value
-            this.HPHero -= this.atk
-        },
-
-        imghero(value) {
-            this.imgHero = value
-            console.log(this.imgHero)
-
-        },
-        imgmonster(value) {
-            this.imgMonster = value
-        },
-
-        $reset(value){
-            
-            this.heroname = value
-            this.monstername=value
-            this.imgHero = value
-            this.imgMonster = value
-
-        },
-        reset(value){
-            this.HPHero = value
-            this.HPMonster = value
-            this.fsizeM_W= 300,
-            this.fsizeH_W = 300,
-            this.fsizeM_H=100,
-            this.fsizeH_H = 100
-        }
-
-    }
-
-}
+  name: "App",
+  components: {
+    HelloWorld,
+    Wbutton,
+  },
+};
 </script>
 
 <style>
-
+#app {
+  background-size: cover;
+  background-image: url(https://thestandard.co/wp-content/uploads/2020/07/THE-POWER-GAME-EP60-COVER-WEB-800x430.jpg);
+  font-family: "Permanent Marker", cursive;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #646464;
+}
 </style>
--- src/components/Wbutton.vue
+++ src/components/Wbutton.vue
@@ -1,58 +1,163 @@
 <template>
-<div id="AttackButton">
-    <button @click="$attk" class="btn btn-warning mr-3">Attack</button>
-    <button @click="$Spattk" class="btn btn-warning">Special Attack</button>
-</div>
+  <div>
+    <button @click="reloadPage()" class="btn btn-dark">Start</button>
+    <div class="row">
+        <div class="col-5">
+          <img style="width:50%" :src="selectedplayer.img"  alt="">
+        </div>
+        <div class="col-2">
+          <div class="row">
+            <br>
+            <br>
+          </div>
+          <div class="row">
+            <img style="width:100%" :src="versus"  alt="">
+          </div>
+        </div>
+        <div class="col-5">
+          <img style="width:50%" :src="selectedmonster"  alt="">
+    </div>
+    </div>
+    <div class="row">
+      <div class="col-5" >
+        Player : {{selectedplayer.name}}
+        <br>
+         <img  v-bind:style="{width: php + 'px'}"  :src="phpbar"  alt="" height="15px">
+         {{php}}
+      </div>
+      <div class="col-5">
+        Monster
+        <br>
+        <img  v-bind:style="{width: mhp + 'px'}"  :src="mhpbar"  alt="" height="15px">
+        {{mhp}}
+      </div>
+    </div>
+    <br/>
+    <br/>
+    <button @click="random()" v-bind:disabled="end"  class="btn btn-dark" >Attack</button>{{space}}
+    <button @click="SPrandom()" v-bind:disabled="end" class="btn btn-dark" >SPAttack</button>
+    <div id="result">
+      <div v-if="mhp==0 || php==0">
+        <div v-if="mhp <php">       
+        Player win
+    </div>
+    <div v-else-if="php < mhp">
+        Monster win
+    </div>
+    <div v-else-if="mhp==php">
+        Draw
+    </div>
+      </div>
+    </div>
+    
+  </div>
 </template>
 
 <script>
 export default {
-    el: '#AttackButton',
-    data: function () {
-        return {
+  data: function () {
+    return {
+      end : false,
+      space : "",
+      phpbar : 'https://i.imgur.com/Q5xIoUa.png',
+      mhpbar : 'https://i.imgur.com/Q5xIoUa.png',
+      versus : 'https://www.freepngimg.com/thumb/animation/84267-versus-of-wallpaper-diagram-computer-witcher-geralt.png',
+        monster : [
+            'https://media0.giphy.com/media/Mir5fnHxvXrTa/giphy.gif',
+            'https://i.pinimg.com/originals/be/17/d2/be17d2237e14cdd73caa8cf1d7e54d09.gif',
+            'https://media3.giphy.com/media/TkDVr6D3OgqEqcLIYP/giphy.gif',
+            {name : "Prayut",img : 'https://www.khaosodenglish.com/wp-content/uploads/2017/04/ten-punch-man.gif'},
+        ],
+        player : [
+          {name : "Thanos",img :'https://data.whicdn.com/images/326805782/original.gif'},
+            {name : "Darth vader",img : 'https://thumbs.gfycat.com/BruisedGrimyGalago-size_restricted.gif'},
+            {name : "Cell",img : 'https://i.makeagif.com/media/10-28-2015/C03MEO.gif'},
             
-        };
+        ],
+        selectedmonster: {name:"",img:null},
+        selectedplayer : {name:"",img:null},
+      ppower: "",
+      mpower: "",
+      php: 250,
+      mhp: 250,
+      term: false,
+    };
+  },
+  methods: {
+    reloadPage : function(){
+    window.location.reload()
+  },
+    random: function () {
+      this.ppower = Math.floor(Math.random() * 7 + 3);
+      this.mhp = this.mhp - this.ppower;
+      if(this.php<=0){
+        this.php = 0
+        this.end = true
+      }else if(this.mhp<=0){
+        this.mhp = 0
+        this.end = true
+      }else if(this.mhp<=0&&this.php<=0){
+        this.mhp = 0
+        this.php = 0
+      }
+      this.mpower = Math.floor(Math.random() * 7 + 3);
+      this.php = this.php - this.mpower;
+      if(this.php<=0){
+        this.php = 0
+        this.end = true
+      }else if(this.mhp<=0){
+        this.mhp = 0
+        this.end = true
+      }else if(this.mhp<=0&&this.php<=0){
+        this.mhp = 0
+        this.php = 0
+      }
     },
-
-    props: {
-        lable: String
+    SPrandom: function () {
+      this.ppower = Math.floor(Math.random() * 10 + 5);
+      this.mhp = this.mhp - this.ppower;
+      if(this.php<=0){
+        this.php = 0
+        this.end = true
+      }else if(this.mhp<=0){
+        this.mhp = 0
+        this.end = true
+      }else if(this.mhp<=0&&this.php<=0){
+        this.mhp = 0
+        this.php = 0
+      }
+      this.mpower = Math.floor(Math.random() * 10 + 5);
+      this.php = this.php - this.mpower;
+      if(this.php<=0){
+        this.php = 0
+        this.end = true
+      }else if(this.mhp<=0){
+        this.mhp = 0
+        this.end = true
+      }else if(this.mhp<=0&&this.php<=0){
+        this.mhp = 0
+        this.php = 0
+      }
     },
-
-    methods:{
-        randomDamage: function (min, max) {
-            
-            return Math.max(Math.floor(Math.random() * max) + 1, min)
-            
-           
-        },
-        $attk : function (){
-            this.atk = this.randomDamage(3,10)
-            this.$emit("atk",this.atk)
-            console.log("Attack :",this.atk)
-
-            this.atk = this.randomDamage(3,10)
-            this.$emit("RandomATK" , this.atk)
-            console.log("Atk back",this.atk)
-        },
-
-        $Spattk : function (){
-            this.spatk = this.randomDamage(10,20)
-            this.$emit("SPATK",this.spatk)
-            console.log("Special Attack :",this.spatk)
-
-            this.spatk = this.randomDamage(10,20)
-            this.$emit("RandomSPATK" , this.spatk)
-            console.log("SPATK back",this.spatk)
-        },
-
-        
-        
-        
-    }
-
-}
+    randommonster (items) {
+      return items[Math.floor(Math.random()*items.length)];
+    },
+    randomplayer (items) {
+      return items[Math.floor(Math.random()*items.length)];
+    },
+  },
+  created() {
+    this.selectedmonster = this.randommonster(this.monster)
+    this.selectedplayer = this.randomplayer(this.player)
+  },
+  
+};
 </script>
 
 <style>
-
+#result{
+  height : 50px;
+  font-size : 200%;
+  color :aliceblue;
+}
 </style>
