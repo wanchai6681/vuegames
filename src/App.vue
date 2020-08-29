12682cbdff04047b75ad0cc379caba62b94892c3
Wanchai Nool <wanchainool@MacBook-Pro-khxng-Wanchai.local>
2020-08-29 23:58:38 +0700

    git

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ export default {
 <style>
 #app {
   background-size: cover;
-  background-image: url();
+  background-image: url(https://thestandard.co/wp-content/uploads/2020/07/THE-POWER-GAME-EP60-COVER-WEB-800x430.jpg);
   font-family: "Permanent Marker", cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
